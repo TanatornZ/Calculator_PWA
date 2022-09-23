067218e24f8b878ed769b8d978f7ce0700c7ce94
TanatornZ <bank250145.bb@gmail.com>
2022-09-23 14:40:22 +0700

    change web icon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,8 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <link rel="apple-touch-icon" href="/images/icons/icon-72x72.png"></link>
+        <link rel="icon" href="/images/icons/icon-72x72.png"/>
         <meta name="theme-color" content="#fff" />
       </Head>
       <title>Calculator</title>
